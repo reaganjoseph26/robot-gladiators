98db5da042b15c7af7e4779c056f1363c21b54f7
Racer <reaganjoseph26@yahoo.com>
2020-10-26 19:45:37 -0400

    one round of battle complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,42 @@
 var playerName = window.prompt("What is your robot's name?");
-//What is this?
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
+var playerHealth = 100;
+var playerAttack = 10;
+//log shorthand = console.log(playerName, playerAttack, playerHealth)
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-// this will do math and log 20
-console.log(10+10);
+var fight = function() {
+    //Alerts players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
 
-//what is this?
-console.log("Our robot's name is " + playerName);
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth = enemyHealth - playerAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health reamaining."
+        );
+    // check enemy's health
+        if (enemyHealth <=0) {
+            window.alert(enemyName + " has died!");
+        }
+        else {
+            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+        }
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+        playerHealth = playerHealth - enemyAttack;
+    // Log a resulting message to the console so we know that it worked
+        console.log(
+            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+        );
+    //check players health
+            if (playerHealth <=0) {
+                window.alert(playerName + " has died!");
+            }
+            else {
+                window.alert(playerName + " still has " + playerHealth + " health left.");
+            }
 
-//window.alert(playerName); //why no quotations for playerName
+};
 
-// this is a custom function named "fight"
-function fight () {
-    window.alert("The Fight has begun");
-}
-//fight();
+fight();
