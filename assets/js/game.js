c13219b0db7012cd6313172269c9ffa6ee7a9145
Racer <reaganjoseph26@yahoo.com>
2020-10-27 16:55:26 -0400

    multiple enemy robot's battle

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,8 +16,8 @@ var enemyAttack = 12;
 
 
 var fight = function(enemyName) {
-    //Alerts players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    // repeat and execute as along as enemy robot is alive
+    while(enemyHealth > 0) {
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     //if player choses to fight, then fight
@@ -68,6 +68,12 @@ var fight = function(enemyName) {
         }
 };
 
+    };
+    
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
-  }
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
+  } 
+
+//fight();
