d4dfc4699f6869fae602b73a5ef2f4175c5475f4
Racer <reaganjoseph26@yahoo.com>
2020-10-30 05:07:00 -0400

    storage of highscore

--- assets/js/game.js
+++ assets/js/game.js
@@ -202,13 +202,19 @@ var startGame = function () {
 
     //function to end game 
     var endGame = function () {
-        // if player is still alive then the player wins.
-        if (playerInfo.health > 0) {
-            window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".")
+        window.alert("The game has ended. Lets see how you did.")
+        //check local storage for high sore.
+        var highScore = localStorage.getItem("highscore");
+        if(highScore === null) {
+            highScore = 0
         }
-        else {
-            window.alert("You've lost your robot in battle.");
-
+        //if player has more than high score, player has new high score
+        if(playerInfo.money > highScore) {
+            localStorage.setItem("highscore", playerInfo.money)
+            localStorage.setItem("name", playerInfo.name);
+            alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+        } else {
+            alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
         }
 
         //ask player if they want to play again
